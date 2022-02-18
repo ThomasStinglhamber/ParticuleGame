d3817b5d0c0a6e783751bafa4b66772697e41148
lorenaclaeys <80050872+lorenaclaeys@users.noreply.github.com>
2022-02-18 23:28:03 +0100

    Update play_set.py

--- play_set.py
+++ play_set.py
@@ -1,57 +1,51 @@
-import pygame as pg
+import pygame, sys
 from pygame.locals import *
-import sys
 from particule_player import Player1
 from particule_player import Player2
 from particule_player import Player3
-pg.init()
 
 
-#generate window
-pg.display.set_caption("Particle game")
-screen = pg.display.set_mode((1380,700))
+pygame.init()
+window = pygame.display.set_mode((1380, 700))
 
-#background
-background = pg.image.load("asset/fond-gris.png")
-
+#colors
+white = (255, 255, 255)
+black = (0, 225, 0)
+red = (255, 0, 0)
+blue = ( 43, 255, 230)
+orange = ( 255, 170, 0)
 
 #load the player
-player1 = Player1("quark down")
-player2 = Player2("quark up")
-player3 = Player3("quark bottom")
+player1 = Player1("quark down", red)
+player2 = Player2("quark up", blue)
+player3 = Player3("quark bottom", orange)
 
-running = True
+run = True
 moving1 = False
 moving2 = False
 moving3 = False
 
-#loop: while the running is true, the window is open
-while running:
-    #background
-    screen.blit(background, (0,0))
-    #player
-    part1 = screen.blit(player1.image, player1.rect)
-    part2  = screen.blit(player2.image, player2.rect)
-    part3  = screen.blit(player3.image, player3.rect)
-    #maj of the screen
-    pg.display.flip()
-    #if the player closes the window
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running == False
-            pg.quit()
+group1 = pygame.sprite.Group()
+group1.add(player1, player2, player3)
+
+while run:
+    #window.blit(background, (0,0))
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
             print("Goodbye!")
             sys.exit()
 
-        #part1
-        elif event.type == MOUSEBUTTONDOWN:
+        #mouse play
+        if event.type == MOUSEBUTTONDOWN:
             if player1.rect.collidepoint(event.pos):
                 moving1 = True
             if player2.rect.collidepoint(event.pos):
                 moving2 = True
             if player3.rect.collidepoint(event.pos):
                 moving3 = True
-
 
         elif event.type == MOUSEBUTTONUP:
               moving1 = False
@@ -65,7 +59,18 @@ while running:
         elif event.type == MOUSEMOTION and moving3:
              player3.rect.move_ip(event.rel)
 
-    hitbox1 = (player1.rect[0] + 20, player1.rect[1], 28, 60)
-    hitbox2 = (player2.rect[0] + 20, player2.rect[1], 28, 60)
-    hitbox3 = (player3.rect[0] + 20, player3.rect[1], 28, 60)
-    pg.sprite.spritecollide(player1, player2, True)
+    #collision
+    collision = pygame.sprite.collide_rect(player1, player2)
+    collision = pygame.sprite.collide_rect(player3, player2)
+    if collision == True:
+        group1.remove(player1,player2,player3)
+        startend = True
+        #group1.add(nouvelle particule)
+        group1.update()
+
+    #aspect of the window
+    window.fill(white)
+    group1.draw(window)
+
+    pygame.display.update()
+
