a01b359a4a900afd20166a59f6ffb2983b0ac53d
lorenaclaeys <80050872+lorenaclaeys@users.noreply.github.com>
2022-02-18 23:42:37 +0100

    Update particule_player.py

--- particule_player.py
+++ particule_player.py
@@ -1,30 +1,10 @@
 import pygame
 
-class Player1(pygame.sprite.Sprite):
-    def __init__(self, particule1, color):
+class Player(pygame.sprite.Sprite):
+    def __init__(self, particule1, color, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([40,40], pygame.SRCALPHA)
         self.rect = self.image.get_rect()#deplacement
-        self.rect.x = 200
-        self.rect.y = 200
-        pygame.draw.circle(self.image, color, (20, 20),20, 0)
-
-
-class Player2(pygame.sprite.Sprite):
-    def __init__(self, particule2,color):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([40,40], pygame.SRCALPHA)
-        self.rect = self.image.get_rect()#deplacement
-        self.rect.x = 500
-        self.rect.y = 200
-        pygame.draw.circle(self.image, color, (20, 20),20, 0)
-
-
-class Player3(pygame.sprite.Sprite):
-    def __init__(self, particule3, color):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([40,40], pygame.SRCALPHA)
-        self.rect = self.image.get_rect()#deplacement
-        self.rect.x = 1000
-        self.rect.y = 250
+        self.rect.x = x
+        self.rect.y = y
         pygame.draw.circle(self.image, color, (20, 20),20, 0)
