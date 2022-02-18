3dbf6e23e86b5522726efbaa7720fe40c4b3fcbb
lorenaclaeys <80050872+lorenaclaeys@users.noreply.github.com>
2022-02-18 23:26:16 +0100

    Update particule_player.py

--- particule_player.py
+++ particule_player.py
@@ -1,29 +1,30 @@
-import pygame as pg
+import pygame
 
-image_part = {"quark up" : ['asset/testparticule.png', 400, 250] , "quark down" : ['asset/testpart.jpeg', 700,250], "quark bottom" : ['asset/quarkb.jpeg', 700,450]}
-class Player1(pg.sprite.Sprite):
-    def __init__(self, particule1):
-        super().__init__()#to call the class init
-        self.image = pg.image.load(image_part[particule1][0])
-        self.image = pg.transform.scale(self.image, (55, 55))
+class Player1(pygame.sprite.Sprite):
+    def __init__(self, particule1, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([40,40], pygame.SRCALPHA)
         self.rect = self.image.get_rect()#deplacement
-        self.rect.x = image_part[particule1][1] #position init
-        self.rect.y = image_part[particule1][2]
+        self.rect.x = 200
+        self.rect.y = 200
+        pygame.draw.circle(self.image, color, (20, 20),20, 0)
 
-class Player2(pg.sprite.Sprite):
-    def __init__(self, particule2):
-        super().__init__()#to call the class init
-        self.image = pg.image.load(image_part[particule2][0])
-        self.image = pg.transform.scale(self.image, (55, 55))
-        self.rect = self.image.get_rect()#deplacement
-        self.rect.x = image_part[particule2][1] #position init
-        self.rect.y = image_part[particule2][2]
 
-class Player3(pg.sprite.Sprite):
-    def __init__(self, particule3):
-        super().__init__()#to call the class init
-        self.image = pg.image.load(image_part[particule3][0])
-        self.image = pg.transform.scale(self.image, (55, 55))
+class Player2(pygame.sprite.Sprite):
+    def __init__(self, particule2,color):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([40,40], pygame.SRCALPHA)
         self.rect = self.image.get_rect()#deplacement
-        self.rect.x = image_part[particule3][1] #position init
-        self.rect.y = image_part[particule3][2]
+        self.rect.x = 500
+        self.rect.y = 200
+        pygame.draw.circle(self.image, color, (20, 20),20, 0)
+
+
+class Player3(pygame.sprite.Sprite):
+    def __init__(self, particule3, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([40,40], pygame.SRCALPHA)
+        self.rect = self.image.get_rect()#deplacement
+        self.rect.x = 1000
+        self.rect.y = 250
+        pygame.draw.circle(self.image, color, (20, 20),20, 0)
